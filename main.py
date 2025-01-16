4b544f71578aaefa2d9458369ddb2b570edcd075
zlu-sh <dev@zlu.sh>
2025-01-16 01:13:07 -0600

    Add char count function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
 
-    print(get_word_count(text))
+    print(get_char_count(text))
 
 def get_word_count(book):
     words = book.split()
@@ -11,6 +11,18 @@ def get_word_count(book):
 def get_book_text(path):
     with open(path) as text:
         return text.read()
+
+def get_char_count(book):
+    lowered_text = book.lower()
+    count_dict = {}
+
+    for char in lowered_text:
+        if char not in count_dict:
+            count_dict[char] = 1
+        else:
+            count_dict[char] += 1
+
+    return count_dict
 
 
 main()
