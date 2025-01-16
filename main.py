707cdd1155755b98ad37f9c203bfc7beabf3f508
zlu-sh <dev@zlu.sh>
2025-01-16 01:03:32 -0600

    Reorder functions

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-def word_count(book):
-    words = book.split()
-    return len(words)
-
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(file_contents)
+    print(word_count(file_contents))
+
+def word_count(book):
+    words = book.split()
+    return len(words)
+
 
 main()
     
